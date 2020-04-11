ec6dd2dc6c3c6b47bf38a3ae4dc73342ffd023de
atwelleric <atwelleric@gmail.com>
2020-04-10 19:46:48 -0500

    completely broke everything, says i have to commit before reverting?

--- homescreen.js
+++ homescreen.js
@@ -1,12 +1,25 @@
-let playButton = document.querySelector('.multiplayer-button');
-const submitButton = document.querySelector('.submit');
-let secretWord = document.querySelector('.secret-word');
+// let playButton = document.querySelector('.multiplayer-button');
+// const submitButton = document.querySelector('.submit');
+// let splitSecretWord = '';
+// let secretWord = document.querySelector('.secret-word');
+// let submittedSecretWord = '';
 
-playButton.addEventListener('click', handlePlayButtonClick);
+// playButton.addEventListener('click', handlePlayButtonClick);
 
-function handlePlayButtonClick() {
-	//transfom screen to display input box, submit botton, and submit instructions. making the rest of the page options disapear.
-	playButton.remove();
-	let hiddenSubmit = document.querySelector('.hidden');
-	hiddenSubmit.classList.toggle('hidden');
-}
+// function handlePlayButtonClick() {
+// 	//transfom screen to display input box, submit botton, and submit instructions. making the rest of the page options disapear.
+// 	playButton.remove();
+// 	let hiddenSubmit = document.querySelector('.hidden');
+// 	hiddenSubmit.classList.toggle('hidden');
+// }
+// submitButton.addEventListener('click', handleSubmitButton);
+
+// function handleSubmitButton() {
+// 	submittedSecretWord = secretWord.value;
+// 	//or once submitted, toggle hidden class onto the input box and submit button. another option is have submit and input on different screen or page.
+// 	// also could set the .value of the input to an empty string after the button submits
+//     splitSecretWord = submittedSecretWord.split('');
+//     // export {splitSecretWord};
+// 	window.location.href = 'gamescreen.html';
+// 	// console.log(splitSecretWord);
+// }
