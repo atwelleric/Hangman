9bc8705508ff13bf65011b6eb1550f9a07029b6d
atwelleric <atwelleric@gmail.com>
2020-04-11 16:02:22 -0500

    saving for loop, trying something new. Also added imgs, everything working, good commit point

--- scripts.js
+++ scripts.js
@@ -4,67 +4,53 @@ const userSelectedLetters = [];
 const onScreenUserSelectedLetters = document.querySelector(
 	'.user-selected-letters'
 );
-const playButton = document.querySelector('.multiplayer-button');
+let wrongGuessNumber = 0;
+//Later, two seperate arrays that will put incorrect letters on one side, correct letters on the other
 const submitButton = document.querySelector('.submit');
-let splitSecretWord = '';
 let secretWord = document.querySelector('.secret-word');
 let submittedSecretWord = '';
-
-
-playButton.addEventListener('click', handlePlayButtonClick);
-
-//Later, two seperate arrays that will put incorrect letters on one side, correct letters on the other
-
-// let splitSecretWord = '';
-// submitButton.addEventListener('click', handleSubmitButton);
-
-// function handleSubmitButton() {
-// 	submittedSecretWord = secretWord.value;
-// 	//cant figure out how to reset the field
-// 	//secretWord.reset();
-// 	//or once submitted, toggle hidden class onto the input box and submit button. another option is have submit and input on different screen or page.
-//     // also could set the .value of the input to an empty string after the button submits
-// 	splitSecretWord = submittedSecretWord.split('');
-// 	console.log(splitSecretWord);
-// }
-function handlePlayButtonClick() {
-	//transfom screen to display input box, submit botton, and submit instructions. making the rest of the page options disapear.
-	playButton.remove();
-	let hiddenSubmit = document.querySelector('.hidden');
-	hiddenSubmit.classList.toggle('hidden');
-}
+let splitSecretWord = '';
 submitButton.addEventListener('click', handleSubmitButton);
 
-function handleSubmitButton(event) {
-    event.preventDefault();
-	window.location.href = 'gamescreen.html';
-    submittedSecretWord = secretWord.value;
-	//or once submitted, toggle hidden class onto the input box and submit button. another option is have submit and input on different screen or page.
-	// also could set the .value of the input to an empty string after the button submits
+function handleSubmitButton() {
+	submittedSecretWord = secretWord.value;
 	splitSecretWord = submittedSecretWord.split('');
-    console.log(splitSecretWord);
+	console.log(splitSecretWord);
+	secretWord.value = ' ';
 }
 
 const keyboard = document.querySelector('.keyboard');
 keyboard.addEventListener('click', handleKeyClick);
 
+if ((handleKeyClick = false)) {
+	console.log('wrong');
+	wrongGuessNumber++;
+}
 function handleKeyClick(event) {
 	userSelectedLetters.push(event.target.innerText);
-	onScreenUserSelectedLetters.innerText = `Letters you chose: ${userSelectedLetters}`;
+	event.target.setAttribute('disabled', true);
+	onScreenUserSelectedLetters.innerText = `Letters you chose: ${userSelectedLetters.join(
+		''
+	)}`;
+	console.log(userSelectedLetters);
+	console.log(wrongGuessNumber);
 	for (let i = 0; i < splitSecretWord.length; i++) {
 		if (event.target.innerText === splitSecretWord[i]) {
+			return true;
 			console.log(splitSecretWord[i]);
 			//can i update .length for only letters that dont exist? remove letters that have returned true from the array?
+			// } else {
+			// 	wrongGuessNumber ++;
+			// 	// if number of wrong returns equals .length, then add body part??
+			// }
 		} else {
-			console.log('wrong');
-			// if number of wrong returns equals .length, then add body part??
+			return false;
 		}
 	}
-	//console.log(userSelectedLetters);
-	//evaluate if the clicked button is equal to any part of the user input string
-	//if the letter is in the string, "reveal" all of those letters in the string
 }
-
+//console.
+//evaluate if the clicked button is equal to any part of the user input string
+//if the letter is in the string, "reveal" all of those letters in the string
 //accept user input and create string
 //if user input contains special characters alert error
 //submit button collects user input and clears input box
@@ -73,4 +59,4 @@ function handleKeyClick(event) {
 //maybe send everything to uppercase automatically to avoid any issues with comparing
 
 //if incorrect option is given, update to different img src
-// when letter is chosen, change display to grey or something
+// when letter is chosen, change display to grey or some thing
